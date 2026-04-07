dec3b713a1ac00c54982c5950b11bfc807c3ce92
Keri <wadayu998@gmail.com>
2026-04-08 02:16:12 +0900

    moved

---
+++ osickogame/.next/dev/server/chunks/ssr/_0u06cs8._.js
@@ -0,0 +1,26 @@
+module.exports = [
+"[project]/app/gameState.ts [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/app/gameState.ts', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/app/styles.module.css [app-ssr] (css module)", ((__turbopack_context__) => {
+
+__turbopack_context__.v({
+});
+}),
+"[project]/app/page.tsx [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+
+const e = new Error("Could not parse module '[project]/app/page.tsx', file not found");
+e.code = 'MODULE_UNPARSABLE';
+throw e;
+}),
+"[project]/node_modules/next/dist/server/route-modules/app-page/vendored/ssr/react-jsx-dev-runtime.js [app-ssr] (ecmascript)", ((__turbopack_context__, module, exports) => {
+"use strict";
+
+module.exports = __turbopack_context__.r("[project]/node_modules/next/dist/server/route-modules/app-page/module.compiled.js [app-ssr] (ecmascript)").vendored['react-ssr'].ReactJsxDevRuntime;
+}),
+];
+
+//# sourceMappingURL=_0u06cs8._.js.map
